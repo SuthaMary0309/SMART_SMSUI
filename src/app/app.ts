1b5d66235f54c03ec1bd45b1537d1a103e4e58f1
Enok111 <venokbilshan@gmail.com>
2025-11-05 09:21:11 +0530

    changes pages

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,17 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { LoginComponent } from "./components/login/login";
+import { RegisterComponent } from "./components/register/register";
+import { StudentDashboardComponent } from "./student-dashboard/student-dashboard";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RegisterComponent, RouterOutlet, StudentDashboardComponent],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrl: './app.css', 
 })
 export class App {
   protected readonly title = signal('smart-sms-school');
 }
+
+
---
+++ src/app/components/login/login.ts
@@ -0,0 +1,22 @@
+import { Component } from '@angular/core';
+import { RegisterComponent } from "../register/register";
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.html',
+  styleUrls: ['./login.css'],
+  imports: [RegisterComponent]
+})
+export class LoginComponent {
+goToRegister() {
+throw new Error('Method not implemented.');
+}
+goToLogin() {
+throw new Error('Method not implemented.');
+}
+  showRegister = false;
+
+  toggleForm() {
+    this.showRegister = !this.showRegister;
+  }
+}
