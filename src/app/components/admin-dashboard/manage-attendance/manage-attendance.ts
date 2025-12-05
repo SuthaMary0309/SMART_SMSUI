ec99a2dd5e50b21e2d989f1bcdce700d935cc86e
Enok111 <venokbilshan@gmail.com>
2025-12-05 14:45:21 +0530

    Manage admin

---
+++ src/app/components/admin-dashboard/manage-attendance/manage-attendance.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-manage-attendance',
+  imports: [],
+  templateUrl: './manage-attendance.html',
+  styleUrl: './manage-attendance.css',
+})
+export class ManageAttendance {
+
+}
